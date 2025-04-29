32eeb2c23c9761c146711523d483efa503cf9ca9
noreply97 <115534973+noreply97@users.noreply.github.com>
2025-04-29 16:29:41 +0200

    modif auth.js

--- dev-front/js/auth.js
+++ dev-front/js/auth.js
@@ -1,47 +1,11 @@
-// js/auth.js
-
-// Inscription
-const registerForm = document.getElementById('register-form');
-if (registerForm) {
-  registerForm.addEventListener('submit', async (e) => {
-    e.preventDefault();
-
-    const username = document.getElementById('username').value.trim();
-    const email = document.getElementById('email').value.trim();
-    const password = document.getElementById('password').value.trim();
-
-    try {
-      const response = await fetch('http://localhost:3000/api/auth/register', {
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json'
-        },
-        body: JSON.stringify({ username, email, password })
-      });
-
-      const data = await response.json();
-
-      if (response.ok) {
-        alert('Registration successful ✅');
-        window.location.href = 'login.html'; // Redirection vers login
-      } else {
-        alert(data.message);
-      }
-
-    } catch (error) {
-      console.error('Error:', error);
-    }
-  });
-}
-
-// Connexion
 const loginForm = document.getElementById('login-form');
+
 if (loginForm) {
   loginForm.addEventListener('submit', async (e) => {
     e.preventDefault();
 
     const email = document.getElementById('email').value.trim();
-    const password = document.getElementById('password').value.trim();
+    const mot_de_passe = document.getElementById('password').value.trim();
 
     try {
       const response = await fetch('http://localhost:3000/api/auth/login', {
@@ -49,21 +13,21 @@ if (loginForm) {
         headers: {
           'Content-Type': 'application/json'
         },
-        body: JSON.stringify({ email, password })
+        body: JSON.stringify({ email, mot_de_passe }) // clé "mot_de_passe" et pas "password"
       });
 
       const data = await response.json();
 
       if (response.ok) {
         localStorage.setItem('token', data.token); // Stocker le token
-        alert('Login successful ✅');
-        window.location.href = 'games.html'; // Redirection vers une page protégée après login
+        alert('Connexion réussie ✅');
+        window.location.href = 'games.html'; // Redirection vers une page protégée
       } else {
-        alert(data.message);
+        alert(data.message); // Affiche le message d'erreur du serveur
       }
 
     } catch (error) {
-      console.error('Error:', error);
+      console.error('Erreur:', error);
     }
   });
 }
